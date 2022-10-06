e06edfa0dfeca63ff77b8b8586b2852f1a60c814
LynnrinChan <wuxiaofei31@gmail.com>
2022-10-06 15:04:20 +0800

    darwin: build the AOSP NFC stack
    Change-Id: I049c285fca52b89c5352985e03af3ecd4bb83291
    Signed-off-by: loserq <2650009628@qq.com>

--- device.mk
+++ device.mk
@@ -64,6 +64,13 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     $(DARWIN_PREBUILT)/kernel/dtb.img:dtb.img
 
+# NFC
+PRODUCT_PACKAGES += \
+    NfcNci \
+    Tag \
+    SecureElement \
+    com.android.nfc_extras
+
 # Overlays
 PRODUCT_PACKAGES += \
     FrameworkResOverlayDarwin \
